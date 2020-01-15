7e606bba13761fe91be39a10fc4da1d842d49471
dingchendong <chendong.ding@d3j.io>
2020-01-15 10:59:40 +0800

    update:init fib-loadbalancer

---
+++ example/index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+let repeater = require("../lib/");
+
+let params = {
+	urls: ["http://127.0.0.1:8093", "http://127.0.0.1:8092", "http://127.0.0.1:8091"],
+	health: "health",
+	response: 'true'
+}
+
+var httpServer = new http.Server(8081, [
+	(req) => {
+		req.session = {}
+	}, {
+		"*": repeater(params)
+	}
+]);
+
+setInterval(() => {
+	console.warn("Server is running", new Date());
+}, 1000 * 5);
+
+httpServer.enableCrossOrigin();
+
+let version = process.version.split("-")[0].split('.')[1];
+if (Number(version) < 29) {
+	httpServer.run(() => {});
+} else {
+	httpServer.start();
+}
